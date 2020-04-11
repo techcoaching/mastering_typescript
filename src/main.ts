722339909088a8c01d84f59cbe7afca87580a9f0
Tu Tran <training@tranthanhtu.vn>
2020-04-11 23:13:27 +0700

    function

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 class Application{
     public static run(){
-        console.log("Hello, this message from application");    
+		console.log("Hello, this message from application");   
+		//console.log("Hello a."+ getName());
+		let name1:string|number=getName();
+		let name2:string|number=10;
+		sayHello("Mr.", name1);
+		sayHello("Mrs.", true);
     }
+}
+
+function sayHello(namePrefix:string, name?:string|number|boolean):void{
+	console.log("void function say hello "+namePrefix+ name);
+}
+function getName():string|number{
+	return 1;
 }
 
 
